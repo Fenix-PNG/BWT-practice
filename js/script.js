3a8bc4dbecfb67e7ec6ca017426da50e7bb68eee
Fenix-PNG <67190573+Fenix-PNG@users.noreply.github.com>
2022-02-21 21:16:42 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+navigator.geolocation.getCurrentPosition (
+    function(position){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=309cfbf55c84bcda344c5c45f47dbe0e`)
+        .then(function (resp) {return resp.json() })
+        .then(function (data) {
+            console.log(data);
+            document.querySelector('.info__name').textContent = data.name;
+            document.querySelector('.info__temp').textContent = Math.round(data.main.temp - 273);
+            document.querySelector('.info__feels').textContent = Math.round(data.main['feels_like'] - 273);
+            document.querySelector('.info__clouds').textContent = data.weather[0]['description'];
+            document.querySelector('.info__img').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+            document.querySelector('.info__speed').textContent = data.wind.speed;
+        })
+    
+        .catch(function () {
 
+        })
+    }
+    )
+        
+    
+  
